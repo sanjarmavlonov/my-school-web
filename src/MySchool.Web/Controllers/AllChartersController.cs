dad5159b5c89e2c4b9ab1f0ec12f471512668b85
zokirjanov <zokirjonovshohruxbek1016@gmail.com>
2023-01-22 21:56:50 +0500

    Created three controllers

---
+++ src/MySchool.Web/Controllers/AllChartersController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MySchool.Services.Common.Utils;
+using MySchool.Services.Interfaces.Services;
+
+namespace MySchool.Web.Controllers
+{
+	public class AllChartersController : Controller
+	{
+		private readonly ICharterService _charterService;
+		private readonly int _pageSize = 20;
+
+		public AllChartersController(ICharterService charterService)
+		{
+			_charterService = charterService;
+		}
+
+		[HttpGet("")]
+		[AllowAnonymous]
+		public async Task<ViewResult> GetAllAsync(int page = 1)
+		{
+			var res =  Ok(await _charterService.GetAll(new PaginationParams(page, _pageSize)));
+			return View(res);
+		}
+	}
+}
---
+++ src/MySchool.Web/Controllers/AllStudentsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MySchool.Services.Common.Utils;
+using MySchool.Services.Interfaces.Services;
+
+namespace MySchool.Web.Controllers
+{
+	public class AllStudentsController : Controller
+	{
+		private readonly IStudentService _studentService;
+		private readonly int _pageSize = 20;
+
+		public AllStudentsController(IStudentService studentService)
+		{
+			_studentService = studentService;
+		}
+
+		[HttpGet("")]
+		[AllowAnonymous]
+		public async Task<ViewResult> GetAllAsync(int page = 1)
+		{
+			var res = Ok(await _studentService.GetAllAsync(new PaginationParams(page, _pageSize)));
+			return View(res);
+		}
+	}
+}
